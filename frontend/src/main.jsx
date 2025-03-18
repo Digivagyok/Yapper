ca83f648c1e71cfcabf94cd9813e4e5a99ebedcf
Digi <marton@szabomarci.hu>
2025-03-18 21:42:22 +0100

    added pwa requirements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,19 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <>
     <App />
